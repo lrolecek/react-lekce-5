9f76546ec1ab6c132445f0e130d1b7be51829dd5
Luděk Roleček <ludek@rolecek.cz>
2022-03-17 19:10:59 +0100

    aktualizace

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+
+
+const names = [
+  'Alena',
+  'Jana',
+  'Emil',
+  'Jindřiška',
+  'Pedro Gonzales',
+];
+
+const jmeno = 'Jirka';
+
+const prijmeniEl = <h2>Novák</h2>;
+
+const mestaEl = [
+  <h3>Praha</h3>,
+  <h3>Brno</h3>,
+  <h3>Ostrava</h3>,
+]
+
 const App = () => {
   return (
     <>
+      <p>{jmeno}</p>
+      { prijmeniEl }
+      { mestaEl }
     </>
   );
 }
