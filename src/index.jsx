f0a66f7c337bd2ff21c0312a174035576d41ae1f
Luděk Roleček <ludek@rolecek.cz>
2022-03-17 19:20:22 +0100

    akt

--- src/index.jsx
+++ src/index.jsx
@@ -3,31 +3,44 @@ import { render } from 'react-dom';
 import './style.css';
 
 
+// const jmeno = 'Jirka';
 
-const names = [
-  'Alena',
-  'Jana',
-  'Emil',
-  'Jindřiška',
-  'Pedro Gonzales',
-];
+// const prijmeniEl = <h2>Novák</h2>;
 
-const jmeno = 'Jirka';
+// const mestaEl = [
+//   <h3>Praha</h3>,
+//   <h3>Brno</h3>,
+//   <h3>Ostrava</h3>,
+// ]
 
-const prijmeniEl = <h2>Novák</h2>;
+// const names = [
+//   'Alena',
+//   'Jana',
+//   'Emil',
+//   'Jindřiška',
+//   'Pedro Gonzales',
+// ];
 
-const mestaEl = [
-  <h3>Praha</h3>,
-  <h3>Brno</h3>,
-  <h3>Ostrava</h3>,
-]
+// const namesEl = names.map(name => <p>{ name }</p>);
+
 
 const App = () => {
   return (
     <>
-      <p>{jmeno}</p>
-      { prijmeniEl }
-      { mestaEl }
+
+
+
+
+
+      {/* <ul>
+        { names.map(name => <li>{ name }</li>) }
+      </ul> */}
+
+      {/*
+        <p>{jmeno}</p>
+        { prijmeniEl }
+        { mestaEl }
+      */}
     </>
   );
 }
